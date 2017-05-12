ce4b4c094b1f8e2541c48466650b2b265afd2cc6
藤﨑 智希 <x16j047@chiba-fjb.ac.jp>
2017-05-12 11:31:20 +0900

    booboo

--- Program.cs
+++ Program.cs
@@ -10,6 +10,21 @@ namespace Example6
     {
         static void Main(string[] args)
         {
+            int score;
+            double pai;
+            String name;
+
+            score = 25;
+            pai = 3.1415;
+            name = "taro";
+
+            Console.Write("SCORE={0} ",score);
+            Console.Write("AVE ={0}",pai);
+            Console.Write("NAME={0} ",name);
+
+
+
+
         }
     }
 }
